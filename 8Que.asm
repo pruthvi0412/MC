c57b751f96092309c0eea5b2f16030db8a7e6cc1
pruthvi0412 <pruthvishetty04@gmail.com>
2025-05-31 19:50:34 +0530

    Create 8Que.asm

---
+++ 8Que.asm
@@ -0,0 +1,12 @@
+        AREA MyCode, CODE, READONLY  
+ENTRY                      
+
+        LDR r1, =0X40000000        
+        LDR r5, [r1]               
+        LDR r6, [r1, #4]            
+        LDR r7, [r1, #8]           
+
+        ADD r2, r5, r6             
+        SUB r2, r2, r7              
+
+        END                         
